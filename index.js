6b00af6373b13240593b589d0405bccfcfeb25e0
Risto Radanliev <ickoice@hotmail.com>
2020-10-25 00:58:27 +0200

    almost complete

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
+const yup = require("yup"); // schema validation
+const monk = require("monk"); // talking to mongoDB
+const { nanoid } = require('nanoid');
+
+require('dotenv').config();
+
+const db = monk(process.env.MONGO_URI);
+const urls = db.get('urls');
+
+urls.createIndex({ slug: 1 }, { unique: true});
 
 const app = express();
 
@@ -11,19 +21,73 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('./public'));
 
-// app.get('/url/:id', (req, res) => {
-//     // TODO: get a short url by id
-// });
+const schema = yup.object().shape({
+    alias: yup.string().trim().matches(/[\w\-]/i),
+    url: yup.string().trim().url().required()
+})
 
-// app.get('/:id', (req, res) => {
-//     // TODO: redirect to url
-// });
+app.get('/:id', async (req, res) => {
 
-// app.post('/url', (req, res) => {
-//     // TODO: create a short url
-// });
+        const {id: slug} = req.params;
+
+        try {
+            const url = await urls.findOne({ slug });
+            
+            if (url) {
+                res.redirect(url.url);
+            }
+            res.redirect(`/?error=${slug} not found`);
+        } catch (error) {
+            res.redirect(`/?error=Link not found`);
+        }
+
+});
+
+app.post('/url', async (req, res, next) => {
+
+    let { slug, url } = req.body;
+
+    try {
+        await schema.validate({
+            slug,
+            url
+        });
+        if (!slug) {
+            slug = nanoid(5);
+        } else {
+            const existing = await urls.findOne({ slug });
+            if (existing) {
+                throw new Error('Slug in use.');
+            }
+        }
+        slug = slug.toLowerCase();
+        const newUrl = {
+            url,
+            slug,
+        };
+        const created = await urls.insert(newUrl);
+        res.json(created);
+    } catch (error) {
+        next(error);
+    }
+    
+});
+
+app.use((error, req, res, next) => {
+
+    if (error.status){
+        res.status(error.status);
+    } else {
+        res.status(500);
+    }
+
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? 'cake' : error.stack,
+    });
+});
 
 const port = process.env.PORT || 1337;
 app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
-})
+});
---
+++ public/app.js
@@ -0,0 +1,27 @@
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        url: '',
+        slug: '',
+        created: null
+    },
+    methods: {
+        async createUrl() {
+            const response = await fetch('/url', {
+                method: 'POST',
+                headers: {
+                    'Content-Type' : 'application/json',
+
+                },
+                body: JSON.stringify({
+                    url: this.url,
+                    slug: this.slug
+                })
+            });
+            this.created = await response.json();
+        }
+    }
+});
+
+console.log(app);
